9e839c968aa9afba9916cf64567dd22869343d28
micropenis <att33pod@gmail.com>
2024-07-24 15:55:08 +0700

    Добавьте файлы проекта.

---
+++ StaltInteractive/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace StaltInteractive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.FSWatch = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.FSWatch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(1, -2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(798, 453);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Stalt Service запущен";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // FSWatch
+            // 
+            this.FSWatch.EnableRaisingEvents = true;
+            this.FSWatch.IncludeSubdirectories = true;
+            this.FSWatch.Path = "C:\\";
+            this.FSWatch.SynchronizingObject = this;
+            this.FSWatch.Changed += new System.IO.FileSystemEventHandler(this.FSWatch_Changed);
+            this.FSWatch.Created += new System.IO.FileSystemEventHandler(this.FSWatch_Created);
+            this.FSWatch.Deleted += new System.IO.FileSystemEventHandler(this.FSWatch_Deleted);
+            this.FSWatch.Renamed += new System.IO.RenamedEventHandler(this.FSWatch_Renamed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Stalt UI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FSWatch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        public System.IO.FileSystemWatcher FSWatch;
+    }
+}
+
---
+++ StaltInteractive/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaltInteractive
+{
+    /*
+     *      (С) Толстопятов Алексей А. 2022.
+     * Мой первый проект File System Watcher -- Stalker Interactive.
+     * Исходный код не изменялся с 14.09.2021. Вот такой я был жопа.
+     * 
+     */
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FSWatch.EnableRaisingEvents = true;
+            textBox1.WordWrap = true;
+            textBox1.TabStop = false;
+            notifyIcon1.Visible = true;
+            notifyIcon1.Text = "Stalker Interactive BP";
+            this.Text = "Stalker Interactive CI";
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+            
+        }
+
+        [STAThread]
+        public void Logging()
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            notifyIcon1.BalloonTipTitle = "Stalker Interactive";
+            notifyIcon1.BalloonTipText = "Приложение запущено";
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.ShowBalloonTip(1000);
+
+            textBox1.Text = "[Система]: Stalker Interactive v 1.0.0.0 \n";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) => 
+            this.WindowState = FormWindowState.Normal;
+        private void FSWatch_Changed(object sender, System.IO.FileSystemEventArgs e) => 
+            textBox1.AppendText($"[  ИЗМЕНЕНО   ]: {e.FullPath} \r\n");
+        private void FSWatch_Created(object sender, System.IO.FileSystemEventArgs e) => 
+            textBox1.AppendText($"[   СОЗДАНО   ]: {e.FullPath} \r\n");
+        private void FSWatch_Deleted(object sender, System.IO.FileSystemEventArgs e) => 
+            textBox1.AppendText($"[   УДАЛЕНО   ]: {e.FullPath} \r\n");
+        private void FSWatch_Renamed(object sender, System.IO.RenamedEventArgs e) =>    
+            textBox1.AppendText($"[ПЕРЕИМЕНОВАНО]: {e.OldFullPath} -> {e.FullPath}\r\n");
+    }
+}
